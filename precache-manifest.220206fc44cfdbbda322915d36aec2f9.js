424183f1aa005eb71cac6f88a402bd311ef3da43
= <sehyun.noh83@gmail.com>
2020-03-22 09:46:52 -0400

    Updates

---
+++ precache-manifest.220206fc44cfdbbda322915d36aec2f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "323563707a41783183dd4d04b9c2822a",
+    "url": "/github-finder-tutorial/home/index.html"
+  },
+  {
+    "revision": "52e597f96c20afad72a5",
+    "url": "/github-finder-tutorial/home/static/css/main.bc1a1419.chunk.css"
+  },
+  {
+    "revision": "3d4a7fb5c0b932514316",
+    "url": "/github-finder-tutorial/home/static/js/2.8feb1a20.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/github-finder-tutorial/home/static/js/2.8feb1a20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52e597f96c20afad72a5",
+    "url": "/github-finder-tutorial/home/static/js/main.96fbfb31.chunk.js"
+  },
+  {
+    "revision": "6f936f8b426fde66054c",
+    "url": "/github-finder-tutorial/home/static/js/runtime-main.7494493c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github-finder-tutorial/precache-manifest.35550647940a3922c465d260e9a86fae.js"
+  "/github-finder-tutorial/home/precache-manifest.220206fc44cfdbbda322915d36aec2f9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-finder-tutorial/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-finder-tutorial/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
